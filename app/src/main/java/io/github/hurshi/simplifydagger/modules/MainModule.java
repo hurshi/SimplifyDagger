15c2f386529e6144f7960333185a00d2a7c56e97
Hurshi <ihsruh@gmail.com>
2019-01-24 19:19:40 +0800

    optimize codes

--- app/src/main/java/io/github/hurshi/simplifydagger/modules/MainModule.java
+++ app/src/main/java/io/github/hurshi/simplifydagger/modules/MainModule.java
@@ -2,13 +2,13 @@ package io.github.hurshi.simplifydagger.modules;
 
 import dagger.Module;
 import dagger.Provides;
-import io.github.hurshi.simplifydagger.scopes.AppScope;
 import io.github.hurshi.simplifydagger.beans.Person;
+import io.github.hurshi.simplifydagger.scopes.ActivityScope;
 
 @Module
 public class MainModule {
 
-    @AppScope
+    @ActivityScope
     @Provides
     Person providePerson() {
         return new Person("hurshi", 18);
