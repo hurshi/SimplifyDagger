d735b9b148015062ec57bd0921e5afde5063c634
Hurshi <ihsruh@gmail.com>
2019-01-30 18:52:31 +0800

    fix AppComponent from kotlin to java

---
+++ sample_daggerandroid/src/main/java/io/github/hurshi/sample_daggerandroid/component/AppComponent.java
@@ -0,0 +1,14 @@
+package io.github.hurshi.sample_daggerandroid.component;
+
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+import io.github.hurshi.sample_daggerandroid.App;
+import io.github.hurshi.sample_daggerandroid.modules.AppModule;
+import io.github.hurshi.sample_daggerandroid.scopes.AppScope;
+import io.github.hurshi.simplifydagger.AutoAndroidActivityScopeComponentInjector;
+
+@AppScope
+@Component(modules = {AppModule.class, AndroidSupportInjectionModule.class, AutoAndroidActivityScopeComponentInjector.class})
+public interface AppComponent extends AndroidInjector<App> {
+}
