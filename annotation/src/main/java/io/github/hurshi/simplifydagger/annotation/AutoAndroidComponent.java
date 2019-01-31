87af5e1dd912d0e95503cea8f6c3dcecc666f91e
Hurshi <ihsruh@gmail.com>
2019-01-31 10:44:32 +0800

    fix @AutoAndroidComponent @Inherited

--- annotation/src/main/java/io/github/hurshi/simplifydagger/annotation/AutoAndroidComponent.java
+++ annotation/src/main/java/io/github/hurshi/simplifydagger/annotation/AutoAndroidComponent.java
@@ -1,10 +1,12 @@
 package io.github.hurshi.simplifydagger.annotation;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Inherited
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.TYPE)
 public @interface AutoAndroidComponent {
